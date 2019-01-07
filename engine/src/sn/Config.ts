49788f9c69e1239df59f33bdc0d12b6ed83438be
famibee <famibee@gmail.com>
2019-01-07 11:25:44 +0900

    テストで引っかかるのでPromise・async/awaitにしない

--- engine/src/sn/Config.ts
+++ engine/src/sn/Config.ts
@@ -136,14 +136,15 @@ export class Config implements IConfig {
 			return;
 		}
 
-		try {
-			(async ()=> {
-				const res = await fetch(sys.cur +'prj.json');
-				load(await res.json());
-			})();
-		} catch (e) {
-			DebugMng.myTrace(`load prj.json "${err_mes}" = %o`, e);
-		}
+		// テストで引っかかるのでPromise・async/awaitにしない
+		fetch(sys.cur +'prj.json')
+		.then(response=> {
+			if (response.ok) return response.json();
+			throw new Error(`load prj.json err = ${response.statusText
+			}`);
+		})		// webpackでバンドルしたら、path.jsonが要らなくなる？
+		.then(load)
+		.catch(err=> DebugMng.myTrace(`load prj.json "${err_mes}" = ${err}`));
 	}
 	private $existsBreakline = false;
 	get existsBreakline(): boolean {return this.$existsBreakline}
