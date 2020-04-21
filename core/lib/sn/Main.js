670c67f765202c0a1aa9a1a7012c965e726cceb0
famibee <famibee@gmail.com>
2020-04-22 00:01:14 +0900

    v0.18.2：bug：複数行タグで行番号が狂う件

--- core/lib/sn/Main.js
+++ core/lib/sn/Main.js
@@ -119,6 +119,12 @@ class Main {
             }
             if (uc == 91) {
                 try {
+                    let cntLines = 0;
+                    let i = -1;
+                    while ((i = token.indexOf('\n', i + 1)) >= 0)
+                        ++cntLines;
+                    if (cntLines > 0)
+                        this.evtMng.cr(cntLines);
                     if (this.scrItr.タグ解析(token)) {
                         this.stop();
                         break;
